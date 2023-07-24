df8de5efcc5d1e8582690aadf133e28c217120f9
MaximFrontend <maximrus62@gmail.com>
2023-07-24 19:12:41 +0300

    создание папки docs

---
+++ docs/js/index.bundle.js
@@ -0,0 +1,1 @@
+!function(){"use strict";const t=document.querySelectorAll('a[href*="#"]');for(let e of t)e.addEventListener("click",(function(t){t.preventDefault();const o=e.getAttribute("href").substr(1);document.getElementById(o).scrollIntoView({behavior:"smooth",block:"start"})}))}();
